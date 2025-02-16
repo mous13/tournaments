6e598721b599b6e591d83ca3fc633d051a076594
Oscar <oscarx1be@gmail.com>
2025-02-16 23:37:03 +0000

    tournaments are now visible in the lists

--- src/Front/Controller/TournamentsCenterController.php
+++ src/Front/Controller/TournamentsCenterController.php
@@ -7,17 +7,29 @@ namespace Vanguard\Tournaments\Front\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Vanguard\Tournaments\Core\Repository\TournamentRepository;
 
 class TournamentsCenterController extends AbstractController
 {
+
+    private const STATUS = [
+        'not_started' => 'Not Started',
+        'preparing'   => 'Preparing',
+        'in_progress' => 'In Progress',
+        'finished'    => 'Finished',
+    ];
+
     public function __construct(
+        private readonly TournamentRepository $tournamentRepository,
     ) {
     }
 
     #[Route('/center', name: 'center')]
     public function __invoke(): Response
     {
+        $tournaments = $this->tournamentRepository->findAll();
         return $this->render('@VanguardTournaments/front/tournaments_center.html.twig', [
+            'tournaments' => $tournaments,
         ]);
     }
 }
